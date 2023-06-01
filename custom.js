7ea9d23fa8a1353060da5094858fd0dc7e2e2dcd
Deborah <soccio.deborah@gmail.com>
2023-06-01 14:25:44 +0200

    ajouts

--- custom.js
+++ custom.js
@@ -22,23 +22,23 @@ function loadDefaultMap() {
   const mapContainer = document.getElementById('map');
   let map = mapContainer.map;
   if (!map) {
-    map = L.map('map').setView([46.603354, 1.888334], 6); // Utilisez les coordonnées et le niveau de zoom souhaités
+    map = L.map('map').setView([46.603354, 1.888334], 6); //  les coordonnées et le niveau de zoom souhaités
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       maxZoom: 18,
     }).addTo(map);
 
-    mapContainer.map = map; // Stocker la référence de la carte dans l'élément HTML pour une utilisation ultérieure
+    mapContainer.map = map; // référence de la carte dans l'élément HTML pour une utilisation ultérieure
 
-    const defaultGpxUrl = 'gpxpoints/track.gpx'; // Remplacez par l'URL du tracé GPX par défaut
+    const defaultGpxUrl = 'gpxpoints/track.gpx'; // l'url du fichier gpx
     loadGPX(defaultGpxUrl, map);
   }
 }
 
-// Fonction pour charger le tracé GPX
+// fonction pour charger le tracé GPX
 function loadGPX(url, map) {
-  // Supprimer les anciennes couches GPX
+  // supprimer les anciens GPX
   if (map.gpxLayer) {
     map.removeLayer(map.gpxLayer);
   }
@@ -57,7 +57,7 @@ function loadGPX(url, map) {
         map.fitBounds(gpxLayer.getBounds());
       }).addTo(map);
 
-      map.gpxLayer = gpxLayer; // Stocker la référence de la couche GPX dans la carte pour une utilisation ultérieure
+      map.gpxLayer = gpxLayer; // on stock la référence de la couche GPX dans la carte pour une utilisation ultérieure
     })
     .catch(error => {
       console.error('Erreur lors du chargement');
@@ -69,7 +69,7 @@ fetch('http://85.169.220.243:5004/api/itineraires')
   .then(data => {
     const itiData = data.data; // Accéder au tableau de données dans la clé "data"
 
-    // Traitement des données, parcours du tableau avec la fonction map
+    // ttt des données, parcours du tableau avec la fonction map
     const itiElements = itiData.map(iti => {
       const id = iti.id;
       const departVille = iti.attributes.depart_ville;
@@ -79,7 +79,7 @@ fetch('http://85.169.220.243:5004/api/itineraires')
       const image = iti.attributes.image_url;
       const gpxUrl = iti.attributes.gpx_url;
 
-      // Utilisation des variables pour construire le code HTML
+      // utilisation des variables pour construire le code HTML
       return `
         <figure class="ficheItineraire" data-gpx="${gpxUrl}">
           <div class="CaRemplaceUneImage"><img src="${image}" alt="illustration"></div>
@@ -104,21 +104,21 @@ fetch('http://85.169.220.243:5004/api/itineraires')
         const mapContainer = document.getElementById('map');
         let map = mapContainer.map;
         if (!map) {
-          map = L.map('map').setView([46.603354, 1.888334], 6); // Utilisez les coordonnées et le niveau de zoom souhaités
+          map = L.map('map').setView([46.603354, 1.888334], 6); //  les coordonnées et le niveau de zoom souhaités
 
           L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
             maxZoom: 18,
           }).addTo(map);
 
-          mapContainer.map = map; // Stocker la référence de la carte dans l'élément HTML pour une utilisation ultérieure
+          mapContainer.map = map; // on stock la référence de la carte dans l'élément HTML pour une utilisation ultérieure
         }
 
         loadGPX(gpxUrl, map);
       });
     });
 
-    // Ajout du gestionnaire d'événements pour afficher la carte par défaut lorsque vous cliquez n'importe où
+    // ajout du gestionnaire d'événements pour afficher la carte par défaut lorsque vous cliquez n'importe où
     document.addEventListener('click', () => {
       loadDefaultMap();
     });
@@ -127,7 +127,7 @@ fetch('http://85.169.220.243:5004/api/itineraires')
     console.error(error);
   });
 
-// Chargement de la carte par défaut au chargement de la page
+// chargement de la carte par défaut au chargement de la page
 loadDefaultMap();
 
 
